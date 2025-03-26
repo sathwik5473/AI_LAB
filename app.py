0174bc49db66d98a8e9b287f75b9266f46fbb3d5
sathwik5473 <sathwiks00007@gmail.com>
2025-03-26 19:44:49 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
 st.title('Loan Approval Predictor')
-model = joblib.load('loan_data.joblib')
+model = joblib.load('loan_data1.joblib')
 gender = st.number_input('Enter gender (M:0, F:1)', min_value=0, max_value=1, step=1)
 married = st.number_input('Enter marital status (Married:1, Unmarried:0)', min_value=0, max_value=1, step=1)
 income = st.number_input('Enter applicant income in thousands', min_value=0.0, step=0.1)
